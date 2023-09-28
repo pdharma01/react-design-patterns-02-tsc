23bc21ff22c793c3aa7b49d140a8e9ce1379d57d
P Dharma <parun.dharma@gmail.com>
2023-09-28 12:06:13 +0700

    second

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,46 @@ import './App.css'
 import SplitScreenContainer from './components/SplitScreenContainer'
 import SplitScreenPanel from './components/SplitScreenPanel'
 
+
+
+let tempData = [
+  {
+    "id": 1,
+    "name": "Smartphone",
+    "price": 499.99,
+    "description": "A powerful mobile device with advanced features.",
+    "rating": 4.5
+  },
+  {
+    "id": 2,
+    "name": "Laptop",
+    "price": 899.99,
+    "description": "A versatile and high-performance computing machine.",
+    "rating": 2.8
+  },
+  {
+    "id": 3,
+    "name": "Smart TV",
+    "price": 699.99,
+    "description": "A large-screen television with streaming capabilities.",
+    "rating": 4.3
+  },
+  {
+    "id": 4,
+    "name": "Wireless Earbuds",
+    "price": 129.99,
+    "description": "Compact earbuds with high-quality audio and wireless convenience.",
+    "rating": 4.7
+  },
+  {
+    "id": 5,
+    "name": "Tablet",
+    "price": 349.99,
+    "description": "A portable touchscreen device for entertainment and productivity.",
+    "rating": 4.2
+  }
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
