8709d80dfcac9da8e53f9c9285f62e7172d6f173
P Dharma <parun.dharma@gmail.com>
2023-10-03 10:46:38 +0700

    list item wip2

--- src/components/LargeListItem.tsx
+++ src/components/LargeListItem.tsx
@@ -1,19 +1,22 @@
 import { ProductType } from "./types"
 
-const LargeListItem = ({ product } :object): React.ReactNode => {
+const LargeListItem = ({ product }): React.ReactNode => {
 
   const { id, name, price, description, rating } = product || {}
 
-  
+
   return (
-    product ?
-      (<div>
+    product ? (
+      
+      <div>
         <h3>{name}</h3>
         <h4>price:${price}, ID:{id}, Stars: {rating} </h4>
         <p>{description} </p>
+      </div>
 
-      </div>) : null
+    ) : null
   )
+
 }
 
 export default LargeListItem
