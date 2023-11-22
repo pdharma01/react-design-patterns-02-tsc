a00854fe78ff0d8d46a0fc8c73b7212bd577809f
pdharma01 <parun.dharma@gmail.com>
2023-11-22 10:52:08 +0700

    clarified wrapping html elements

--- src/components/basicTypes/WrappingHTMLElement.tsx
+++ src/components/basicTypes/WrappingHTMLElement.tsx
@@ -1,14 +1,18 @@
 type ButtonProps = {
     variant: "primary" | "secondary",
-    children: string
-} & Omit<React.ComponentProps<"button">, "children">
+    children: string  //restrict children type to string, not div or other
+} & Omit<React.ComponentProps<"button">, "children"> 
+
+//access html components children and ...rest with  -- & React.ReactComponentProps<button> --(in this case html "button" type to add onClick)
+
+// children is default to ReactNode. By adding custom type children:string , children will then become union of ReactNode and string. To restrict to only string, use--- & Omit<  > ---to omit default children, then add my own children:string
 
 const WrappingHTMLElement = ({ variant, children, ...rest }: ButtonProps) => {
     return (
 
         <div className="component-section">
             <h4>Wrapping HTML Element into React.ComponentProps</h4>
-            <p>Add "& React.ReactComponentProps(button)" to have access to children and ...rest of props</p>
+            <p>Add "& React.ReactComponentProps(button)" to have access to html button children and ...rest of props</p>
             <p>Key word "Omit" to limit to only string children</p>
             <button
                 className={`${variant}-color-style`}
